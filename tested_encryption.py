45f75ffacbc26c24bd58dce86798207abfa9eae1
Chirag Arti Sharma <chip2741@gmail.com>
2023-10-18 16:17:59 +0530

    Update tested_encryption.py

--- tested_encryption.py
+++ tested_encryption.py
@@ -4,11 +4,11 @@ from datetime import datetime
 from collections import defaultdict
 import botocore
 import logging
-from prettytable import PrettyTable
 
 VOLUME_DETAILS_LIST = []
 PENDING_SNAPSHOTS = []
 FAILED_SNAPSHOTS = []
+EXCLUDED_INSTANCES = ['instance-id-1', 'instance-id-2']  # Add your instance IDs here
 
 logging.basicConfig(filename='script_logs.log', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger()
@@ -60,14 +60,18 @@ def get_volume_info(session):
 
 def create_snapshot(session, volume_id):
     ec2_client = session.client("ec2")
+    start_time = datetime.now()
     response = ec2_client.create_snapshot(VolumeId=volume_id)
     snapshot_id = response['SnapshotId']
     waiter = ec2_client.get_waiter('snapshot_completed')
     robust_waiter(waiter, SnapshotIds=[snapshot_id])
+    elapsed_time = datetime.now() - start_time
+    logger.info(f"Snapshot {snapshot_id} created in {elapsed_time}")
     return snapshot_id
 
 def create_encrypted_volume(session, snapshot_id, availability_zone, size, volume_type, kms_key):
     ec2_client = session.client("ec2")
+    start_time = datetime.now()
     response = ec2_client.create_volume(
         SnapshotId=snapshot_id,
         AvailabilityZone=availability_zone,
@@ -79,6 +83,8 @@ def create_encrypted_volume(session, snapshot_id, availability_zone, size, volum
     volume_id = response['VolumeId']
     waiter = ec2_client.get_waiter('volume_available')
     robust_waiter(waiter, VolumeIds=[volume_id])
+    elapsed_time = datetime.now() - start_time
+    logger.info(f"Encrypted volume {volume_id} created in {elapsed_time}")
     return volume_id
 
 def attach_encrypted_volume(session, encrypted_volume_id, instance_id, device_name):
@@ -117,17 +123,8 @@ def start_instance(session, instance_id):
     logger.info(f"Instance {instance_id} started in {elapsed_time}")
 
 def log_volume_details(details):
-    VOLUME_DETAILS_LIST.append(details)
-
-def write_volume_details_to_file():
-    table = PrettyTable()
-    table.field_names = ["Old Volume ID", "New Volume ID", "Instance ID", "Instance Name", "Device Name", "Disk Size", "Snapshot ID", "Availability Zone"]
-    
-    for detail in VOLUME_DETAILS_LIST:
-        table.add_row([detail["old_volume_id"], detail["new_volume_id"], detail["instance_id"], detail["instance_name"], detail["device_name"], detail["disk_size"], detail["snapshot_id"], detail["availability_zone"]])
-
-    with open(f'volume_changes_{datetime.now().strftime("%Y%m%d%H%M%S")}.csv', 'w') as file:
-        file.write(table.get_string())
+    with open(f'volume_changes_{datetime.now().strftime("%Y%m%d%H%M%S")}.csv', 'a') as file:
+        file.write(f"{details['old_volume_id']},{details['new_volume_id']},{details['instance_id']},{details['instance_name']},{details['device_name']},{details['disk_size']},{details['snapshot_id']},{details['availability_zone']}\n")
 
 def process_volumes_for_instance(session, volumes, kms_key):
     instance_id = volumes[0]['Attachments'][0]['InstanceId']
@@ -135,34 +132,23 @@ def process_volumes_for_instance(session, volumes, kms_key):
 
     for volume in volumes:
         volume_id = volume['VolumeId']
-        try:
-            snapshot_id = create_snapshot(session, volume_id)
-            encrypted_volume_id = create_encrypted_volume(session, snapshot_id, volume['AvailabilityZone'], volume['Size'], volume['VolumeType'], kms_key)
-            detach_volume(session, volume_id)
-            attach_encrypted_volume(session, encrypted_volume_id, instance_id, volume['Attachments'][0]['Device'])
+        snapshot_id = create_snapshot(session, volume_id)
+        encrypted_volume_id = create_encrypted_volume(session, snapshot_id, volume['AvailabilityZone'], volume['Size'], volume['VolumeType'], kms_key)
+        detach_volume(session, volume_id)
+        attach_encrypted_volume(session, encrypted_volume_id, instance_id, volume['Attachments'][0]['Device'])
 
-            instance_name = get_instance_name(session, instance_id)
-            details = {
-                'old_volume_id': volume_id,
-                'new_volume_id': encrypted_volume_id,
-                'instance_id': instance_id,
-                'instance_name': instance_name,
-                'device_name': volume['Attachments'][0]['Device'],
-                'disk_size': volume['Size'],
-                'snapshot_id': snapshot_id,
-                'availability_zone': volume['AvailabilityZone']
-            }
-            log_volume_details(details)
-        except botocore.exceptions.WaiterError:
-            PENDING_SNAPSHOTS.append({
-                'volume_id': volume_id,
-                'instance_id': instance_id,
-                'availability_zone': volume['AvailabilityZone'],
-                'size': volume['Size'],
-                'volume_type': volume['VolumeType'],
-                'kms_key': kms_key
-            })
-            logger.error(f"Snapshot creation for volume {volume_id} took too long. Adding to pending snapshots list.")
+        instance_name = get_instance_name(session, instance_id)
+        details = {
+            'old_volume_id': volume_id,
+            'new_volume_id': encrypted_volume_id,
+            'instance_id': instance_id,
+            'instance_name': instance_name,
+            'device_name': volume['Attachments'][0]['Device'],
+            'disk_size': volume['Size'],
+            'snapshot_id': snapshot_id,
+            'availability_zone': volume['AvailabilityZone']
+        }
+        log_volume_details(details)
 
     start_instance(session, instance_id)
 
@@ -191,16 +177,17 @@ def process_pending_snapshots(session):
             PENDING_SNAPSHOTS.remove(completed_snapshot)
 
         if PENDING_SNAPSHOTS:
-            logger.info(f"Retrying processing of pending snapshots. Attempt {retry_count + 1} of {MAX_RETRIES}...")
             retry_count += 1
 
     if PENDING_SNAPSHOTS:
-        logger.error(f"Failed to process some snapshots even after {MAX_RETRIES} retries.")
         for failed in PENDING_SNAPSHOTS:
             logger.error(f"Failed snapshot details: {failed}")
 
 def main():
     start_time = datetime.now()
+
+    with open(f'volume_changes_{datetime.now().strftime("%Y%m%d%H%M%S")}.csv', 'a') as file:
+        file.write("Old Volume ID,New Volume ID,Instance ID,Instance Name,Device Name,Disk Size,Snapshot ID,Availability Zone\n")
 
     session = create_session()
     kms_key = get_kms_key_arn(session)
@@ -223,17 +210,16 @@ def main():
     zones_order = list(encountered_zones)
 
     for zone in zones_order:
-        logger.info(f"Processing volumes in availability zone: {zone}")
         instance_to_volumes_map = zone_to_instance_to_volumes_map[zone]
 
         for instance_id, volumes in instance_to_volumes_map.items():
+            if instance_id in EXCLUDED_INSTANCES:
+                logger.info(f"Skipping encryption process for excluded instance: {instance_id}")
+                continue
             process_volumes_for_instance(session, volumes, kms_key)
 
     if PENDING_SNAPSHOTS:
-        logger.info("Processing pending snapshots...")
         process_pending_snapshots(session)
-
-    write_volume_details_to_file()
 
     elapsed_time = datetime.now() - start_time
     logger.info(f"Script completed in {elapsed_time}")
