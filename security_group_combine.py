60aaa398afe6dd53bc6baad352b55ebf5d63a2a4
Chirag Arti Sharma <chip2741@gmail.com>
2023-11-23 18:50:28 +0530

    Update security_group_combine.py

--- security_group_combine.py
+++ security_group_combine.py
@@ -16,6 +16,7 @@ def get_security_group_rules(sg_ids):
         for permission in sg_info['SecurityGroups'][0]['IpPermissions']:
             if not any(permission == existing_rule for existing_rule in rules):
                 rules.append(permission)
+    print(rules)
     return rules
 
 def create_security_group(vpc_id, sg_name):
@@ -34,16 +35,18 @@ def add_rules_to_security_group(sg_id, rules):
     existing_rules = sg_info['SecurityGroups'][0]['IpPermissions']
 
     rules_to_add = [rule for rule in rules if not check_duplicate_rules(existing_rules, rule)]
-
+    added_rule_count = 0
     for rule in rules_to_add:
         try:
             ec2_client.authorize_security_group_ingress(GroupId=sg_id, IpPermissions=[rule])
             print(f"Rule added to {sg_id}.")
+            added_rule_count+=1
         except ec2_client.exceptions.ClientError as e:
             if e.response['Error']['Code'] == 'InvalidPermission.Duplicate':
                 print(f"Duplicate rule, skipped: {rule}")
             else:
                 print(f"Failed to add rule to {sg_id}: {e}")
+    print(f"Total number of rules added to {sg_id}: {added_rule_count}")
 
 
 def user_confirmation(prompt):
@@ -53,22 +56,22 @@ def main():
     sg_ids = input("Enter the security group IDs to combine, separated by commas: ").split(',')
     combined_rules = get_security_group_rules(sg_ids)
 
-    if len(combined_rules) > 195 and user_confirmation("Rule count exceeds 195. Create a new security group? (yes/no): "):
-        vpc_id = input("Enter your VPC ID: ")
-        sg_name = input("Enter the name for the new security group: ")
-        new_sg_id = create_security_group(vpc_id, sg_name)
-        add_rules_to_security_group(new_sg_id, combined_rules[:195])
+    if len(combined_rules) > 195:
+        user_choice = input("Rule count exceeds 195. Would ypu like to create a new security group or use an existing one? (new/existing): ").lower()
+        
+        if user_choice == 'new':
+            vpc_id = input("Enter your VPC ID: ")
+            sg_name = input("Enter the name for the new security group: ")
+            new_sg_id = create_security_group(vpc_id, sg_name)
+            add_rules_to_security_group(new_sg_id, combined_rules[:195])
+        elif user_choice == 'existing':
+            existing_sg_id = input("Enter the existing security group ID: ")
+            add_rules_to_security_group(existing_sg_id, combined_rules[:195])
+        else: 
+            print("Invalid input. Existing the script.")
     else:
         existing_sg_id = input("Enter the existing security group ID to add the rules: ")
         add_rules_to_security_group(existing_sg_id, combined_rules[:195])
 
 if __name__ == '__main__':
     main()
-
-
-# [ec2-user@ip-10-140-241-119 sg-combine]$ python3 sg-combine.py
-# Enter the security group IDs to combine, separated by commas: sg-00d655c03893e4563,sg-0cc133920bb6d45cb
-# Enter the existing security group ID to add the rules: sg-0b70ab9c598da470c
-# Rule added to sg-0b70ab9c598da470c.
-# Failed to add rule to sg-0b70ab9c598da470c: An error occurred (InvalidPermission.Duplicate) when calling the AuthorizeSecurityGroupIngress operation: the specified rule "peer: 104.115.39.0/24, TCP, from port: 443, to port: 443, ALLOW" already exists
-
